67751b1a3061e03000d9604b85df8b3c154f1168
Alex Rafter <alexrafteremail@gmail.com>
2022-11-02 21:02:17 +0000

    icons as svg

--- js/shortlist-plus/components/LocalHeart.js
+++ js/shortlist-plus/components/LocalHeart.js
@@ -3,7 +3,20 @@ import { store } from "./Store.js";
 export function LocalHeart(props) {
   return {
     $template: /*html*/ `
-          <span v-cloak ref="props.data.url" style="cursor:pointer;" @click.prevent="toggleShortlist" :class="carObj.addedToList === false ? 'local-heart icon icon-heart1 ${props.class}' : 'local-heart icon icon-heart2 ${props.class}'" :data-select-msg="carObj.addedToList === false ? 'select' : 'selected'" @vue:mounted="addToMountedArrAndSet()"></span>`,
+          <span 
+          v-cloak 
+          ref="props.data.url" 
+          style="cursor:pointer;" 
+          @click.prevent="toggleShortlist" 
+          class="local-heart"
+          :class="props.class"
+          :data-select-msg="carObj.addedToList === false ? 'select' : 'selected'" 
+          @vue:mounted="addToMountedArrAndSet()"
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+              <path :d="carObj.addedToList ? carObj.heartFullSVGPath : carObj.heartEmptySVGPath"/>
+            </svg>
+          </span>`,
     //
     carObj: {
       url: props.data.url,
@@ -14,6 +27,8 @@ export function LocalHeart(props) {
       price: props.data.price,
       image: props.data.image,
       addedToList: false,
+      heartEmptySVGPath: 'm8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z',
+      heartFullSVGPath: 'M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z',
     },
     // Next Job : Add All Below to Store and Tweak Code 
     //
